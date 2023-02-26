72f07edfb0691f40a701f2191269f06318673449
westonh2 <weston@full-circle.design>
2023-02-26 12:46:05 -0500

    scroll video fix

--- js/scroll-3.js
+++ js/scroll-3.js
@@ -1,39 +1,70 @@
-(function(){
+// (function(){
 
-    var throttle = function(type, name, obj){
-        var obj = obj || window;
-        var running = false;
-        var func = function(){
-        if (running){ return; }
-        running = true;
-        requestAnimationFrame(function(){
-            obj.dispatchEvent(new CustomEvent(name));
-            running = false;
-        });
-        };
-        obj.addEventListener(type, func);
-    };
+//     var throttle = function(type, name, obj){
+//         var obj = obj || window;
+//         var running = false;
+//         var func = function(){
+//         if (running){ return; }
+//         running = true;
+//         requestAnimationFrame(function(){
+//             obj.dispatchEvent(new CustomEvent(name));
+//             running = false;
+//         });
+//         };
+//         obj.addEventListener(type, func);
+//     };
     
-    throttle("scroll", "optimizedScroll");
-    })();
+//     throttle("scroll", "optimizedScroll");
+// })();
 
 
+var pause = false;
+var videoLoaded = false;
+n = .2;
+var video = document.getElementById("cow-video");
+video.onloadeddata = function() {
+    windowHeight = window.innerHeight*9;
+    console.log('video is loaded');
+    videoLoaded = true;
+    videoLength = video.duration;
+    i = videoLength/windowHeight;
+}
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    vid = document.getElementById('cow-video');
-    vLen = vid.duration;
+    //videoLength = video.duration;
 
-    h = window.innerHeight*9;
+    windowHeight = window.innerHeight*9;
 
-    i = vLen/h;
+    //i = videoLength/windowHeight;
 })
 
 
 window.addEventListener("scroll", function() {
+    // if (pause == false) {
+    //     scrollPos = window.scrollY;
+    //     n = scrollPos*i;
+    //     video.currentTime = n;
+    //     console.log('current time: '+n);
+    //     pause = true;
+    //     setTimeout(() => {
+    //         pause = false;
+    //     }, 200);
+    // }
+
+    if (videoLoaded == false) {
+        return;
+    }
     scrollPos = window.scrollY;
     n = scrollPos*i;
-    vid.currentTime = n;
+    nCalculation = n - video.currentTime;
+    if (nCalculation < -0.1 || nCalculation > 0.1) {
+        //console.log('n calculation: '+nCalculation);
+        video.currentTime = n;
+        //console.log('current time: '+n);
+    }
+    
 })
 
+// if last n value is too close to current, don't fire
 
